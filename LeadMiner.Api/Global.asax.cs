a8bf895327e86111a967f5f8a1527c929931bdfc
Jason Ody <jasonody@users.noreply.github.com>
2016-10-27 22:25:12 -0700

    server side retrieval of Facebook access token

--- LeadMiner.Api/Global.asax.cs
+++ LeadMiner.Api/Global.asax.cs
@@ -7,6 +7,7 @@ using System.Web.Routing;
 using System.Web.Security;
 using System.Web.SessionState;
 using System.Web.Http;
+using Newtonsoft.Json.Serialization;
 
 namespace LeadMiner.Api
 {
@@ -17,7 +18,11 @@ namespace LeadMiner.Api
             // Code that runs on application startup
             AreaRegistration.RegisterAllAreas();
             GlobalConfiguration.Configure(WebApiConfig.Register);
-            RouteConfig.RegisterRoutes(RouteTable.Routes);            
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+
+			HttpConfiguration config = GlobalConfiguration.Configuration;
+			config.Formatters.JsonFormatter.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+			config.Formatters.JsonFormatter.UseDataContractJsonSerializer = false;
         }
     }
 }
